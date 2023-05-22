81066a3ee315132aa61d95108e0ebab8fb06fe8d
Bruno Noobador <51370142+Bruno-Noobador@users.noreply.github.com>
2023-05-22 04:38:23 +0000

    added rna to aminoacid dict and function

--- dna2nukeacid.py
+++ dna2nukeacid.py
@@ -6,8 +6,76 @@ dna_to_rna = {'A':'U',
               'G':'C',
               'T':'A'}
 
+rna_to_aminoacid = {'UUU': 'Phe',
+                    'UUC': 'Phe',
+                    'UUA': 'Leu',
+                    'UUG': 'Leu',
+                    'UCU': 'Ser',
+                    'UCC': 'Ser',
+                    'UCA': 'Ser',
+                    'UCG': 'Ser',
+                    'UAU': 'Tyr',
+                    'UAC': 'Tyr',
+                    'UAA': 'Stop',
+                    'UAG': 'Stop',
+                    'UGU': 'Cys',
+                    'UGC': 'Cys',
+                    'UGA': 'Stop',
+                    'UGG': 'Trp',
+
+                    'CUU': 'Leu',
+                    'CUC': 'Leu',
+                    'CUA': 'Leu',
+                    'CUG': 'Leu',
+                    'CCU': 'Pro',
+                    'CCC': 'Pro',
+                    'CCA': 'Pro',
+                    'CCG': 'Pro',
+                    'CAU': 'His',
+                    'CAC': 'His',
+                    'CAA': 'Gln',
+                    'CAG': 'Gln',
+                    'CGU': 'Arg',
+                    'CGC': 'Arg',
+                    'CGA': 'Arg',
+                    'CGG': 'Arg',
+                    
+                    'AUU': 'Ile',
+                    'AUC': 'Ile',
+                    'AUA': 'Ile',
+                    'AUG': 'Met',
+                    'ACU': 'Thr',
+                    'ACC': 'Thr',
+                    'ACA': 'Thr',
+                    'ACG': 'Thr',
+                    'AAU': 'Asn',
+                    'AAC': 'Asn',
+                    'AAA': 'Lys',
+                    'AAG': 'Lys',
+                    'AGU': 'Ser',
+                    'AGC': 'Ser',
+                    'AGA': 'Arg',
+                    'AGG': 'Arg',
+                    
+                    'GUU': 'Val',
+                    'GUC': 'Val',
+                    'GUA': 'Val',
+                    'GUG': 'Val',
+                    'GCU': 'Ala',
+                    'GCC': 'Ala',
+                    'GCA': 'Ala',
+                    'GCG': 'Ala',
+                    'GAU': 'Asp',
+                    'GAC': 'Asp',
+                    'GAA': 'Glu',
+                    'GAG': 'Glu',
+                    'GGU': 'Gly',
+                    'GGC': 'Gly',
+                    'GGA': 'Gly',
+                    'GGG': 'Gly'}
+
 def generate_random_DNA(length: int) -> str:
-    random_sequence:str = ''.join(choice(DNALETTERS) for i in range(length))
+    random_sequence:str = ''.join(choice(DNALETTERS) for i in range(length*3))
     return random_sequence
 
 def irna_tor(DnaInput: str) -> str:
@@ -15,12 +83,25 @@ def irna_tor(DnaInput: str) -> str:
     RNAOutput: str = ''.join([dna_to_rna.get(nucleotide) for nucleotide in DnaInput])
     return RNAOutput
 
-DnaSequence: str = generate_random_DNA(5)
+def amino_acid_inator(RnaInput: str) -> str:
+    ProperlySeparatedRNA = []
+    for n in range(0, len(RnaInput), 3):
+        ProperlySeparatedRNA.append(''.join(RnaInput[n:n+3]))
+    
+    print(f"ProperlySeparatedRNA: {ProperlySeparatedRNA}")
+
+    AminoacidOutput: str = ' '.join([rna_to_aminoacid.get(aminoacid) for aminoacid in ProperlySeparatedRNA])
+    return AminoacidOutput
+
+DnaSequence: str = generate_random_DNA(10)
 
 UpperCasedDNA: str = DnaSequence.upper()
 
-print(UpperCasedDNA)
+print(f"DnaSequence: {UpperCasedDNA}")
 
 RnaSequence: str = irna_tor(UpperCasedDNA)
 
-print(RnaSequence)
+print(f"RnaSequence: {RnaSequence}")
+
+NucleicAcids:str = amino_acid_inator(RnaSequence)
+print(f"NucleicAcids: {NucleicAcids}")
